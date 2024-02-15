a1d223122b074abf9b618567e537f347978e4836
carmona <carmonajca@gmail.com>
2024-02-15 13:29:24 +0000

    Patched missing global variables

--- llama/generation.py
+++ llama/generation.py
@@ -44,6 +44,9 @@ Dialog = List[Message]
 B_INST, E_INST = "[INST]", "[/INST]"
 B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
 
+SPECIAL_TAGS = [B_INST, E_INST, "<<SYS>>", "<</SYS>>"]
+UNSAFE_ERROR = "Error: special tags are not allowed as part of the prompt."
+
 
 class LLaMA:
     def __init__(self, model: Transformer, tokenizer: Tokenizer):
